9aea5edd71211c1865787f3835b09e61a5a899cb
johnateofilo-ops <john.a.teofilo@gmail.com>
2026-08-08 14:11:52 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration for this deployment.
+// NOTE: these values initialize the Firebase Web SDK and are not secret.
+// The app reads `window.firebaseConfig` at startup to initialize Auth.
+window.firebaseConfig = {
+	apiKey: "AIzaSyBUeyQZ2Sv2r-E36WA9d8S-3t0-E8rJ2L4",
+	authDomain: "coffee-pos-c2780.firebaseapp.com",
+	databaseURL: "https://coffee-pos-c2780-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "coffee-pos-c2780",
+	storageBucket: "coffee-pos-c2780.firebasestorage.app",
+	messagingSenderId: "495911754530",
+	appId: "1:495911754530:web:ef07b266de20120db1a37b",
+	measurementId: "G-GXNS6LWRLV"
+};
